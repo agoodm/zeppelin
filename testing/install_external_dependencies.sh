03c6c4fc1467fc0630f1e217110f8cd9a686a8dd
Alex Goodman <agoodm@users.noreply.github.com>
2016-11-11 01:32:23 -0800

    Travis

--- testing/install_external_dependencies.sh
+++ testing/install_external_dependencies.sh
@@ -21,22 +21,22 @@
 set -e
 
 if [[ -v "$PYTHON" ]] then
-    if [[ "$PYTHON" == "2.7" ]] then
-      wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
-    else
-      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    fi
-    bash miniconda.sh -b -p $HOME/miniconda
-    export PATH="$HOME/miniconda/bin:$PATH"
-    hash -r
-    conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
-    conda info -a
-    conda config --add channels conda-forge
-    conda install -q python=$PYTHON matplotlib pandasql
+  if [[ "$PYTHON" == "2.7" ]] then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
   else
-    mkdir -p ~/R
-    echo 'R_LIBS=~/R' > ~/.Renviron
-    R -e "install.packages('knitr', repos = 'http://cran.us.r-project.org', lib='~/R')"
-    export R_LIBS='~/R'
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+  fi
+  bash miniconda.sh -b -p $HOME/miniconda
+  export PATH="$HOME/miniconda/bin:$PATH"
+  hash -r
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
+  conda info -a
+  conda config --add channels conda-forge
+  conda install -q python=$PYTHON matplotlib pandasql
+else
+  mkdir -p ~/R
+  echo 'R_LIBS=~/R' > ~/.Renviron
+  R -e "install.packages('knitr', repos = 'http://cran.us.r-project.org', lib='~/R')"
+  export R_LIBS='~/R'
 fi
