bc65c750ff9945884f8180fc42789af20e537ff6
Alex Goodman <agoodm@users.noreply.github.com>
2016-11-12 00:33:07 -0800

    Travis

--- spark/src/main/java/org/apache/zeppelin/spark/PySparkInterpreter.java
+++ spark/src/main/java/org/apache/zeppelin/spark/PySparkInterpreter.java
@@ -176,8 +176,8 @@ public class PySparkInterpreter extends Interpreter implements ExecuteResultHand
     Map env = EnvironmentUtils.getProcEnvironment();
     if (!env.containsKey("PYTHONPATH")) {
       SparkConf conf = getSparkConf();
-      env.put("PYTHONPATH", conf.get("spark.submit.pyFiles").replaceAll(",", ":"));
-      env.put("PYTHONPATH", "../interpreter/lib/python:$PYTHONPATH");
+      env.put("PYTHONPATH", conf.get("spark.submit.pyFiles").replaceAll(",", ":") + 
+              ":../interpreter/lib/python");
     }
     return env;
   }
